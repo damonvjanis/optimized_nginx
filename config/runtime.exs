15b06f78d527bb240ab695284771666e6e75c3a4
Damon Janis <damonvjanis@gmail.com>
2023-04-06 09:28:58 -0600

    Adjust runtime to use expected env var

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,11 +1,8 @@
 import Config
 
-# Set ports based on environment variables in the release
-http_port = System.get_env("HTTP_PORT")
-https_port = System.get_env("HTTPS_PORT")
+# Set port based on environment variables in the release
+port = System.get_env("PORT")
 
-if http_port && https_port do
-  config :my_app, MyAppWeb.Endpoint,
-    http: [port: String.to_integer(http_port)],
-    https: [port: String.to_integer(https_port)]
+if port do
+  config :my_app, MyAppWeb.Endpoint, http: [port: String.to_integer(port)]
 end
